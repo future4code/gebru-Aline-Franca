e37b130a9ef633f6f0ffdb16e916be2e8279408f
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2021-11-09 22:34:06 -0300

    A atualização do arquivo foram as atividades propostas

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,48 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+//   RESPOSTA EXERCÍCIO 1:
+    /*console.log(b) 10
+    console.log(a,b) 10, 5
+    */
+
+//    RESPOSTA EXERCÍCIO 2:
+    //console.log(a, b, c) 10, 10, 10
+
+//RESPOSTA EXERCÍCIO 3:
+//   let horasTrabalhadasDia = prompt("Quantas horas você trabalha por dia?")
+//    let receitaPorDia = prompt("Quanto você recebe por dia?")
+//    alert('Voce recebe ${receitaPorDia/horasTrabalhadasDia} por hora')
+
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+// 1. 
+
+// a)
+// let nameUser 
+// 1. b)
+// let idadeUser
+// 1. c)
+// console.log (typeof nameUser)
+// console.log (typeof idadeUser)
+// 1. d) Esse typeof foi impresso como undefined porque eu não atribuí nenhum valor.
+// 1. e) e f)
+// console.log (typeof prompt("Qual é o seu nome?"))
+// console.log (typeof prompt ("Quantos anos você tem?"))
+// Desta vez o typeof imprimiu string, não só para o nome do usuário como para a sua idade. Prompt sempre imprimi string.
+// g)
+
+// 2.
+
+// 3. 
+
+let a = 10
+let b = 25
+c = a // 10
+console.log (a, b, c)
+a = b
+console.log (a, b, c)
+b = c
+console.log (a, b)
+
+
