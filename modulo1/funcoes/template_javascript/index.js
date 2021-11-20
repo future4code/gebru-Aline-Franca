f1dc01766fb9ee215aecdd5079d18305678f3400
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2021-11-19 22:31:09 -0300

    Exercícios de funções

---
+++ modulo1/funcoes/template_javascript/index.js
@@ -0,0 +1,110 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// 1. 
+// function minhaFuncao(variavel) {
+// 	return variavel * 5
+// }
+
+// console.log(minhaFuncao(2))
+// console.log(minhaFuncao(10))
+
+// a)10 e 50
+// b) Não apareceria nada, pois para aparecer no console precisamos executar o console.log
+
+
+// 2.
+// a) Transforma todos os caracteres do texto inserido pelo usuário em letras minúsculas e verifica se no texto possui a palavra "cenoura"
+
+// b. Determine qual será a saída no console para cada uma das 3 entradas do usuário:
+   
+   // i.   `Eu gosto de cenoura`
+   /*"eu gosto de cenoura"
+   true
+   */
+
+   // ii.  `CENOURA é bom pra vista`
+   /*"cenoura é bom pra vista"
+   true
+   */
+
+   // iii. `Cenouras crescem na terra`
+   /*"cenouras crescem na terra"
+   true
+   */
+
+   
+   // EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+   // 1.
+   // a)
+   // console.log("Eu sou Aline, tenho 26 anos, moro em Araruama e sou estudante.")
+
+   // b)
+   
+//    function UserInformation(nameUser,age,city,profession) {
+//        const frase = "Eu sou " + nameUser + ", tenho "+ age + " anos, moro em " + city + " e sou " + profession + "."
+//        return frase   
+//     }
+//    console.log(UserInformation("Aline", 26, "Araruama", "auxiliar administrativo"))
+
+// 2. 
+// a)
+
+// function soma(number1, number2) {
+//     const resultado = number1 + number2
+//     return resultado
+// }
+// console.log(soma(2,8))
+
+// b)
+// function compararNumeros(number1, number2) {
+//    const comparar = number1 >= number2
+//    return comparar 
+// }
+// console.log (compararNumeros(5,9))
+
+// c)
+
+// d)
+// let frase1 = "Eu amo praia!"
+
+// function message(frase) {
+//     frase = frase.length()
+//     frase = frase.toUpperCase()
+    
+//     return frase
+// }
+// console.log(message(frase1))
+
+// 3.
+
+// Number3 = prompt("Insira um número:")
+// Number4 = prompt("Insira outro número:")
+
+// function somar(Number3, Number4) {
+//     const soma = Number3 + Number4
+//     return soma
+// }
+
+// function diminuir(Number3, Number4){
+//     const diferenca = Number3 - Number4
+//     return diferenca
+// }
+
+// function multiplicar(Number3, Number4){
+//     const multiplicacao = Number3 * Number4
+//     return multiplicacao
+// }
+
+// function dividir(Number3, Number4){
+//     const divisao = Number3 / Number4
+//     return divisao
+// }
+// somar()
+// diminuir()
+// multiplicar()
+// dividir()
+
+
+// console.log("Números inseridos: " + Number3 + " e " + Number4)
+// console.log("Soma:" + somar())
