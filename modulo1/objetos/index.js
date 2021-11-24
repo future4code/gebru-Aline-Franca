e9543b8831f925e9ce608b603633c82b8a7fe7fd
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2021-11-23 21:43:06 -0300

    Exercícios Objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,52 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// 1.
+
+// a)
+// console.log(filme.elenco[0]) -----------------Impresso no console: Matheus Nachtergaele
+// console.log(filme.elenco[filme.elenco.length - 1]) ---------- Impresso no console: 
+// console.log(filme.transmissoesHoje[2]) -----------Impresso no console: Canal: Globo, Horário: 14h
+
+ // 2.
+
+ // a)
+
+//  console.log(cachorro)
+//  Juca
+//  3
+//  SRD 
+
+// console.log(gato)
+// Juba
+// 3
+// SRD 
+
+// console.log(tartaruga)
+// Jubo
+// 3
+// SRD 
+
+// b)
+// Ela copia os dados do objeto mencionado após os três pontos.
+
+// 3.
+
+// a)
+// false
+// undefined
+
+// b)
+// Na primeira opção ele retornou false porque foi o valor informado no objeto. Já a variável altura não havia sido declarada, por isso o undefined
+
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+// 1.
+
+// const informacoesPessoa = {
+//     nome: "Aline",
+//     apelidos: ['line', 'gisele', 'branquela']
+// }
+
+// console.log('Eu sou' ,$[informacoesPessoa.nome], 'mas pode me chamar de' ,$[informacoesPessoa.apelidos])
+
