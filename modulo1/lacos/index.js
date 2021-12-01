6f454d2347d9be2ceb002d3d93750567932a377e
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2021-11-30 22:54:40 -0300

    Exercícios laços

---
+++ modulo1/lacos/index.js
@@ -0,0 +1,25 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+// 1.
+
+
+// .
+
+// a)
+// Todos os números maiores que 18:
+// [19, 21, 23, 25, 27, 30]
+
+// b)
+
+// 3.
+
+// Quatro linhas com asterístico, começando com um e termninando com 4 asterísticos, pois a cada o comando
+// pede para acrescentar mais 1.
+
+// EXERCÍCIOS DE ESCRITA DE CÓDIGO
+
+// 1.
+
+let quantidadePets = Number(prompt("Quantos pets você tem na sua casa?"))
+
+// 2. 
