52c03ecb67e087fb02d8e0ef72b9f1ffc1cc4348
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-02-06 23:55:46 -0300

    MVP WhatsLab

--- modulo3/projeto-whatslab/src/App.js
+++ modulo3/projeto-whatslab/src/App.js
@@ -1,25 +1,88 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled from "styled-components"
 
-function App() {
+const Pagina = styled.div`
+display: flex;
+justify-content: center;
+`
+const Whatslab = styled.div`
+max-width: 600px;
+height: 100vh;
+border: 1px solid black;
+flex: 1 1 0%;
+display: flex;
+flex-direction: column;
+`
+const BoxMensagem = styled.div`
+flex: 1 1 0%;
+display: flex;
+flex-direction: column;
+-webkit-box-pack: end;
+justify-content: flex-end;
+padding: 20px;
+`
+const CaixaMensagem = styled.div`
+display: flex;
+`
+const CaixaInputUsuario = styled.input`
+`
+
+const CaixaInputMensagem = styled.input`
+flex-grow: 1;
+`
+
+export default class App extends React.Component {
+  state = {
+    inputUsuario: "",
+    inputMensagem: ""
+  };
+  onChangeInputUsuario = (event) => {
+    this.setState({ inputUsuario: event.target.value });
+    };
+
+  onChangeInputMensagem = (event) => {
+    this.setState({ inputMensagem: event.target.value });
+    };  
+
+  limparInputs = () => {
+    console.log("Usuário: ", this.state.inputUsuario);
+    console.log("Mensagem: ", this.state.inputMensagem);
+      
+  
+  this.setState({
+    inputUsuario: "",
+    inputMensagem: "",
+    });   
+  };
+
+render() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+  <Pagina>
+  <Whatslab>
+  <BoxMensagem>
+  
+  </BoxMensagem>
+  
+  <CaixaMensagem>
+  <CaixaInputUsuario   
+  placeholder="Usuário"
+  value={this.state.inputUsuario}
+  onChange={this.onChangeInputUsuario}
+  /> 
+
+  <CaixaInputMensagem
+  placeholder="Mensagem"
+  value={this.state.inputMensagem}
+  onChange={this.onChangeInputMensagem}
+  />
+  
+  <button onClick={this.limparInputs}>Enviar</button>
+  
+  </CaixaMensagem>
+  </Whatslab>
+  </Pagina> 
+  </> 
   );
 }
-
-export default App;
+}
