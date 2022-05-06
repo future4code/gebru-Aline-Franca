d2a3ec3f2cc958d75c60d72d8cbce274a7b895a0
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-05-05 22:21:32 -0300

    Parte 4 - Detalhes do personagem

--- revisao-js/star-wars-projeto/src/pages/CharacterDetailsPage/CharacterDetailsPage.jsx
+++ revisao-js/star-wars-projeto/src/pages/CharacterDetailsPage/CharacterDetailsPage.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import axios from 'axios'
+
 
 function CharacterDetailsPage (props){
+    const [details, setDetails] = useState({})
+
+    useEffect(()=>{
+        GetDetailsPage()  
+       },[props.url])
+
+    function GetDetailsPage(){
+        axios.get(props.url)
+        .then((response)=> {
+            console.log(response.data)
+            setDetails(response.data)})
+        .catch((error)=>{
+            console.log(error.message)
+        })
+    }
+
     return(
     <>    
     <h1>Detalhes do Personagem</h1>
-    <p>{props.url}</p>
+    <p>Nome: {details.name}</p>
+    <p>Planeta de Origem: {details.homeworld}</p>
     <button onClick={props.goToListPage}>Voltar para a lista de personagens</button>
     </>
     )
