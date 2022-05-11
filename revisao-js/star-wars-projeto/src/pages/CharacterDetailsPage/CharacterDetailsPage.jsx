22367401bf7848bdac22a71a0e7e6c9e0dbcd507
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-05-10 22:33:34 -0300

    Continuação Parte 4

--- revisao-js/star-wars-projeto/src/pages/CharacterDetailsPage/CharacterDetailsPage.jsx
+++ revisao-js/star-wars-projeto/src/pages/CharacterDetailsPage/CharacterDetailsPage.jsx
@@ -4,11 +4,17 @@ import axios from 'axios'
 
 function CharacterDetailsPage (props){
     const [details, setDetails] = useState({})
+    const [planets, setPlanets] = useState({})
 
     useEffect(()=>{
         GetDetailsPage()  
        },[props.url])
 
+    useEffect(()=>{
+        GetPlanetsDetails()
+    }, [details.homeworld]) 
+    
+    
     function GetDetailsPage(){
         axios.get(props.url)
         .then((response)=> {
@@ -19,11 +25,20 @@ function CharacterDetailsPage (props){
         })
     }
 
+    function GetPlanetsDetails(){
+        axios.get(details.homeworld)
+        .then((response) => {
+            console.log(response.data)
+            setPlanets(response.data)
+        })
+        .catch((error) =>console.log(error.message))
+    }
+
     return(
     <>    
     <h1>Detalhes do Personagem</h1>
     <p>Nome: {details.name}</p>
-    <p>Planeta de Origem: {details.homeworld}</p>
+    <p>Planeta de Origem: {planets.name}</p>
     <button onClick={props.goToListPage}>Voltar para a lista de personagens</button>
     </>
     )
