647cd016872256aa61dd62ce2d30c5b7c4fd48d6
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-05-29 23:58:37 -0300

    Exercícios 1 e 2

---
+++ modulo5/lista-typescript-projeto/src/index.ts
@@ -0,0 +1,20 @@
+// EXERCÍCIOS TYPESCRIPT
+
+//EXERCÍCIO 01
+
+// function usersInfo(nome:string, birthDate:string){
+//     const data = birthDate.split("/")
+    
+//     const frase = `Olá me chamo ${nome}, nasci no dia ${data[0]} do mês ${data[1]} do ano de ${data[2]}.`
+//     console.log(frase)
+// }
+// usersInfo("Aline", "28/09/1995")
+
+//EXERCÍCIO 02
+
+// function recebeParametro (numeroAleatorio:number){
+//     const resultado = typeof numeroAleatorio
+//     console.log(`O tipo da variável recebida nesta função é ${resultado}`)
+// }
+// recebeParametro(6)
+
