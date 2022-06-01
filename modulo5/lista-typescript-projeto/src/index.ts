f499f223f3faa03ec1b2472ccea4c6da75cd1283
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-05-31 22:04:11 -0300

    Exercícios 4 e 5

--- modulo5/lista-typescript-projeto/src/index.ts
+++ modulo5/lista-typescript-projeto/src/index.ts
@@ -20,28 +20,99 @@
 
 //EXERCÍCIO 03
 
-enum GENERO {
-	ACAO="ação",
-	DRAMA="drama",
-	COMEDIA="comédia",
-	ROMANCE="romance",
-	TERROR="terror"
+// enum GENERO {
+// 	ACAO="ação",
+// 	DRAMA="drama",
+// 	COMEDIA="comédia",
+// 	ROMANCE="romance",
+// 	TERROR="terror"
+// }
+
+// type filmes= {
+//     nomeFilme: string,
+//     anoLancamentoFilme: number,
+//     generoFilme: string,
+//     pontuacaoFilme?: number
+// }
+
+// function filmesInfo (nome:string, anoLancamento:number, genero:string, pontuacao?:number){
+//     const filmes:filmes = {
+//         nomeFilme: nome,
+//         anoLancamentoFilme: anoLancamento,
+//         generoFilme: genero,
+//         pontuacaoFilme:pontuacao
+//     }
+//     console.log(filmes) 
+//     }   
+//     filmesInfo("Homem-Aranha", 2022, GENERO.ACAO, 90)
+
+// EXERCÍCIO 4
+
+// let array = [
+// 	{ nome: "Marcos", salário: 2500, setor: "marketing", presencial: true },
+// 	{ nome: "Maria" ,salário: 1500, setor: "vendas", presencial: false},
+// 	{ nome: "Salete" ,salário: 2200, setor: "financeiro", presencial: true},
+// 	{ nome: "João" ,salário: 2800, setor: "marketing", presencial: false},
+// 	{ nome: "Josué" ,salário: 5500, setor: "financeiro", presencial: true},
+// 	{ nome: "Natalia" ,salário: 4700, setor: "vendas", presencial: true},
+// 	{ nome: "Paola" ,salário: 3500, setor: "marketing", presencial: true }
+// ]
+
+// enum Setores {
+//     MARKETING = "marketing",
+//     VENDAS = "vendas",
+//     FINANCEIRO = "financeiro"
+// }
+
+// type pessoas = {
+//     nome: string,
+//     salário: number,
+//     setor: Setores,
+//     presencial: boolean
+// }
+
+// const retornaMarketing =(listaFuncionarios:pessoas[]) =>{
+//     return listaFuncionarios.filter((funcionario:any)=>funcionario.setor===Setores.MARKETING && funcionario.presencial)
+// }
+
+
+
+
+// EXERCÍCIO 5
+
+let usersArray = [
+	{name: "Rogério", email: "roger@email.com", role: "user"},
+	{name: "Ademir", email: "ademir@email.com", role: "admin"},
+	{name: "Aline", email: "aline@email.com", role: "user"},
+	{name: "Jéssica", email: "jessica@email.com", role: "user"},  
+	{name: "Adilson", email: "adilson@email.com", role: "user"},  
+	{name: "Carina", email: "carina@email.com", role: "admin"}      
+] 
+
+type users ={
+    name: string,
+    email: string,
+    role: string
 }
 
-type filmes= {
-    nomeFilme: string,
-    anoLancamentoFilme: number,
-    generoFilme: string,
-    pontuacaoFilme?: number
-}
+function selecionaAdmin (array:users[]){
+    const admin = array.filter((ad)=>{
+        return ad.role === "admin"
 
-function filmesInfo (nome:string, anoLancamento:number, genero:string, pontuacao?:number){
-    const filmes:filmes = {
-        nomeFilme: nome,
-        anoLancamentoFilme: anoLancamento,
-        generoFilme: genero,
-        pontuacaoFilme:pontuacao
-    }
-    console.log(filmes) 
-    }   
-    filmesInfo("Homem-Aranha", 2022, GENERO.ACAO, 90)
+    }).map(ad=>{
+        return ad.email
+    })
+
+}
+selecionaAdmin(usersArray)
+
+// EXERCÍCIO 6
+
+let clientesBanco = [
+	{ cliente: "João", saldoTotal: 1000, debitos: [100, 200, 300] },
+	{ cliente: "Paula", saldoTotal: 7500, debitos: [200, 1040] },
+	{ cliente: "Pedro", saldoTotal: 10000, debitos: [5140, 6100, 100, 2000] },
+	{ cliente: "Luciano", saldoTotal: 100, debitos: [100, 200, 1700] },
+	{ cliente: "Artur", saldoTotal: 1800, debitos: [200, 300] },
+	{ cliente: "Soter", saldoTotal: 1200, debitos: [] }
+]
