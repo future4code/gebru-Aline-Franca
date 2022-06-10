8f81f5779013f59ead5f5799c2b3d08f9524f0a1
Aline Cristina da Silva Leite França <cristina.aline21@gmail.com>
2022-06-09 23:33:44 -0300

    Atualização aprofundamento express

--- modulo5/aprofundamento-express/src/index.ts
+++ modulo5/aprofundamento-express/src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 import {Request, Response} from "express"
+import { afazeres } from "./data";
+import { v4 as generateId } from 'uuid';
 
 
 
@@ -9,6 +11,29 @@ import {Request, Response} from "express"
 const app = express(); //habilita o express
 app.use(express.json()); //forma que comunicaremos json
 app.use(cors())
+
+
+// EXERCÍCIO 01
+// app.get("/ping", (req: Request, res: Response) => {          
+//     res.status(200).send("Pong! 🏓")
+// })
+
+// ------------------------------------------
+
+
+app.get("/afazeres", (req:Request, res: Response)=>{
+    try{
+        const allTarefas = afazeres
+
+        res.status(200).send(afazeres)
+    }
+    
+    catch(error){
+        res.status(400).end("Caso de erro")
+    }
+})
+
+
 
 
 
